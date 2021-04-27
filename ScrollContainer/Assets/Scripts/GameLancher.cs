cdb193b5a510f1a9907351196309ae7ae444d40e
huantang0529@gmail.com <huantang0529@gmail.com>
2021-04-27 18:13:11 +0800

    1. 滚动容器初版提交

--- ScrollContainer/Assets/Scripts/GameLancher.cs
+++ ScrollContainer/Assets/Scripts/GameLancher.cs
@@ -12,11 +12,37 @@ using UnityEngine;
 /// GameLancher.cs
 /// 游戏启动脚本
 /// </summary>
+[DisallowMultipleComponent]
 public class GameLancher : MonoBehaviour
 {
+    /// <summary>
+    /// 单例对象
+    /// </summary>
+    public static GameLancher Singleton;
+
+    /// <summary>
+    /// UI根节点
+    /// </summary>
+    [Header("UI根节点")]
+    public Transform UIRoot;
+
+    /// <summary>
+    /// UI挂载节点
+    /// </summary>
+    [Header("UI挂载节点")]
+    public Transform UIParent;
+
     void Start()
     {
-        DontDestroyOnLoad(gameObject);  
+        if(Singleton == null)
+        {
+            Singleton = this;
+        }
+        else
+        {
+            Debug.LogError($"重复挂载GameLancher脚本,请检查代码!");
+        }
+        DontDestroyOnLoad(gameObject);
     }
 
 
